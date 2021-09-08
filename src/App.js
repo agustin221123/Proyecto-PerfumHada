c991427bf3aacb424231d0987913b0d2affcc6af
agu avila <agu5avilaprieto@gmail.com>
2021-09-08 20:17:06 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import NavBar from './components/NavBar';
 
-function App() {
+
   return (
     <div className="App">
       <header className="App-header">
